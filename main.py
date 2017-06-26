4c52a2af43632abffc8598680c904030e3c62593
Genaron <gslaymuns@uc.cl>
2017-06-26 18:03:36 -0400

    test

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
 import os
-import mi_bot
+import logging
+import requests
+from mi_bot import start, get, post, label, close
 from flask import Flask
 
+
+token_bot = '439953184:AAFdqVKqmxFOnKuClHhjCPN0v0Ee1ZmMwZw'
+url = 'https://api.telegram.org/bot{}/'.format(token_bot)
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - '
+						   '%(message)s', level=logging.INFO)
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-	return 'El servidor está arriba!'
+	return get_updates()
+
+
+def send_msg(text, chat_id):
+	global url
+	url_msg = url + 'sendMessage?text={}&chat_id={}'.format(text, chat_id)
+
+
+def get_updates():
+	global url
+	url_updates = url + 'getUpdates'
+	updates = requests.get(url_updates).json()
+	return updates
+
 
 if __name__ == '__main__':
 	port = int(os.environ.get('PORT', 8080))
--- mi_bot.py
+++ mi_bot.py
@@ -2,163 +2,136 @@ import re
 import logging
 
 from github_api import get_issue, post_comment, add_label, close_issue
-from telegram.ext import (Updater, CommandHandler)
-
-
-token_bot = '439953184:AAFdqVKqmxFOnKuClHhjCPN0v0Ee1ZmMwZw'
-# url = 'https://api.telegram.org/bot{}/'.format(token_bot)
-updater = Updater(token=token_bot)
-dispatcher = updater.dispatcher
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - '
-                           '%(message)s', level=logging.INFO)
 
 
 def start(bot, update):
-    text = 'Hola {}!\n'.format(update.message.from_user.first_name)
-    text += 'Soy PrograBot! Con mi ayuda, podrás manejar algunas funcional'
-    text += 'idades de los issues presentes en el repositorio https://github.co'
-    text += 'm/Genaron/T07\n-------------\nCOMANDOS:\n/get #num_issue:'
-    text += 'Se obtiene la información asociada a la issue correspondiente.\n'
-    text += '/post #num_issue *respuesta: Se responde la issue correspondiente '
-    text += 'con la respuesta entregada.\n/label #num_issue *label: Se asigna l'
-    text += 'a etiqueta indicada al issue correspondiente.\n/close #num_issue: '
-    text += 'se cierra la issue indicada.\n-------------\nNota: no escribir el '
-    text += '\'#\' ni el \'*\' al momento de llamar a los comandos anteriores.'
-    bot.send_message(chat_id=update.message.chat_id, text=text)
+	text = 'Hola {}!\n'.format(update.message.from_user.first_name)
+	text += 'Soy PrograBot! Con mi ayuda, podrás manejar algunas funcional'
+	text += 'idades de los issues presentes en el repositorio https://github.co'
+	text += 'm/Genaron/T07\n-------------\nCOMANDOS:\n/get #num_issue:'
+	text += 'Se obtiene la información asociada a la issue correspondiente.\n'
+	text += '/post #num_issue *respuesta: Se responde la issue correspondiente '
+	text += 'con la respuesta entregada.\n/label #num_issue *label: Se asigna l'
+	text += 'a etiqueta indicada al issue correspondiente.\n/close #num_issue: '
+	text += 'se cierra la issue indicada.\n-------------\nNota: no escribir el '
+	text += '\'#\' ni el \'*\' al momento de llamar a los comandos anteriores.'
+	bot.send_message(chat_id=update.message.chat_id, text=text)
 
 
 def cmd_error(bot, update):
-    text = 'El comando ingresado no es válido. Use /start para ver la lista '
-    text += 'de comandos.'
-    bot.send_message(chat_id=update.message.chat_id, text=text)
+	text = 'El comando ingresado no es válido. Use /start para ver la lista '
+	text += 'de comandos.'
+	bot.send_message(chat_id=update.message.chat_id, text=text)
 
 
 def get(bot, update):
-    comando = ''
-    for s in 'get':
-        comando += '({}|{})'.format(s, s.upper())
-    pattern = '^/{} \d+$'.format(comando)
+	comando = ''
+	for s in 'get':
+		comando += '({}|{})'.format(s, s.upper())
+	pattern = '^/{} \d+$'.format(comando)
 
-    msg = update.message.text
-    if bool(re.match(pattern, msg)):
-        issue_id = msg.split(' ')[1]
-        items = get_issue(issue_id)
-        if items is None:
-            text = 'No existe la issue {}'.format(issue_id)
-        else:
-            state, numero, autor, titulo, texto, url = items
-            if state == 'closed':
-                text = 'La issue {} ya fue resuelta.\n\n'.format(issue_id)
-            else:
-                text = 'La issue {} no ha sido resuelta.\n\n'.format(issue_id)
-            text += '[autor: {}]\n\n'.format(autor)
-            text += '[#{} - {}]\n\n'.format(numero, titulo)
-            text += texto + '\n\n' + url
-        bot.send_message(chat_id=update.message.chat_id, text=text)
-    else:
-        cmd_error(bot, update)
+	msg = update.message.text
+	if bool(re.match(pattern, msg)):
+		issue_id = msg.split(' ')[1]
+		items = get_issue(issue_id)
+		if items is None:
+			text = 'No existe la issue {}'.format(issue_id)
+		else:
+			state, numero, autor, titulo, texto, url = items
+			if state == 'closed':
+				text = 'La issue {} ya fue resuelta.\n\n'.format(issue_id)
+			else:
+				text = 'La issue {} no ha sido resuelta.\n\n'.format(issue_id)
+			text += '[autor: {}]\n\n'.format(autor)
+			text += '[#{} - {}]\n\n'.format(numero, titulo)
+			text += texto + '\n\n' + url
+		bot.send_message(chat_id=update.message.chat_id, text=text)
+	else:
+		cmd_error(bot, update)
 
 
 def post(bot, update):
-    user = update.message.from_user.first_name
+	user = update.message.from_user.first_name
 
-    comando = ''
-    for s in 'post':
-        comando += '({}|{})'.format(s, s.upper())
-    pattern = '^/{} \d+ (\s|\S)+'.format(comando)
+	comando = ''
+	for s in 'post':
+		comando += '({}|{})'.format(s, s.upper())
+	pattern = '^/{} \d+ (\s|\S)+'.format(comando)
 
-    msg = update.message.text
-    if bool(re.match(pattern, msg)):
-        space_1 = msg.find(' ')
-        space_2 = msg.find(' ', space_1 + 1)
-        issue_id = msg[space_1 + 1: space_2]
-        respuesta = msg[space_2 + 1:]
-        items = get_issue(issue_id)
-        if items is None:
-            text = 'No existe la issue {}'.format(issue_id)
-        elif items[0] == 'closed':
-            text = 'La issue {} ya está cerrada.'.format(issue_id)
-        else:
-            signature = '\n\n----------\nRespuesta generada por {} desde ' \
-                        '@GenaroLaymunsBot de Telegram.'.format(user)
-            status = post_comment(issue_id, respuesta + signature)
-            if status == 201:
-                text = 'Issue número {} respondida con éxito! Para ver el ' \
-                       'issue con los comentarios, ingrese a https://github' \
-                       '.com/Genaron/T07/issues/{}'.format(issue_id, issue_id)
-            else:
-                text = 'No fue posible comentar el issue.'
-        bot.send_message(chat_id=update.message.chat_id, text=text)
-    else:
-        cmd_error(bot, update)
+	msg = update.message.text
+	if bool(re.match(pattern, msg)):
+		space_1 = msg.find(' ')
+		space_2 = msg.find(' ', space_1 + 1)
+		issue_id = msg[space_1 + 1: space_2]
+		respuesta = msg[space_2 + 1:]
+		items = get_issue(issue_id)
+		if items is None:
+			text = 'No existe la issue {}'.format(issue_id)
+		elif items[0] == 'closed':
+			text = 'La issue {} ya está cerrada.'.format(issue_id)
+		else:
+			signature = '\n\n----------\nRespuesta generada por {} desde ' \
+						'@GenaroLaymunsBot de Telegram.'.format(user)
+			status = post_comment(issue_id, respuesta + signature)
+			if status == 201:
+				text = 'Issue número {} respondida con éxito! Para ver el ' \
+					   'issue con los comentarios, ingrese a https://github' \
+					   '.com/Genaron/T07/issues/{}'.format(issue_id, issue_id)
+			else:
+				text = 'No fue posible comentar el issue.'
+		bot.send_message(chat_id=update.message.chat_id, text=text)
+	else:
+		cmd_error(bot, update)
 
 
 def label(bot, update):
-    comando = ''
-    for s in 'label':
-        comando += '({}|{})'.format(s, s.upper())
-    pattern = '^/{} \d+ \w+$'.format(comando)
+	comando = ''
+	for s in 'label':
+		comando += '({}|{})'.format(s, s.upper())
+	pattern = '^/{} \d+ \w+$'.format(comando)
 
-    msg = update.message.text
-    if bool(re.match(pattern, msg)):
-        space_1 = msg.find(' ')
-        space_2 = msg.find(' ', space_1 + 1)
-        issue_id = msg[space_1 + 1: space_2]
-        new_label = msg[space_2 + 1:]
+	msg = update.message.text
+	if bool(re.match(pattern, msg)):
+		space_1 = msg.find(' ')
+		space_2 = msg.find(' ', space_1 + 1)
+		issue_id = msg[space_1 + 1: space_2]
+		new_label = msg[space_2 + 1:]
 
-        items = get_issue(issue_id)
-        if items is None:
-            text = 'No existe la issue {}'.format(issue_id)
-        elif items[0] == 'closed':
-            text = 'La issue {} ya está cerrada.'.format(issue_id)
-        else:
-            text = add_label(issue_id, new_label)
-            text += ' Para ver el issue correspondiente, ingrese a ' \
-                    'https://github.com/Genaron/T07/issues/{}' \
-                    ''.format(issue_id)
-        bot.send_message(chat_id=update.message.chat_id, text=text)
-    else:
-        cmd_error(bot, update)
+		items = get_issue(issue_id)
+		if items is None:
+			text = 'No existe la issue {}'.format(issue_id)
+		elif items[0] == 'closed':
+			text = 'La issue {} ya está cerrada.'.format(issue_id)
+		else:
+			text = add_label(issue_id, new_label)
+			text += ' Para ver el issue correspondiente, ingrese a ' \
+					'https://github.com/Genaron/T07/issues/{}' \
+					''.format(issue_id)
+		bot.send_message(chat_id=update.message.chat_id, text=text)
+	else:
+		cmd_error(bot, update)
 
 
 def close(bot, update):
-    comando = ''
-    for s in 'close':
-        comando += '({}|{})'.format(s, s.upper())
-    pattern = '^/{} \d+$'.format(comando)
+	comando = ''
+	for s in 'close':
+		comando += '({}|{})'.format(s, s.upper())
+	pattern = '^/{} \d+$'.format(comando)
 
-    msg = update.message.text
-    if bool(re.match(pattern, msg)):
-        issue_id = msg.split(' ')[1]
+	msg = update.message.text
+	if bool(re.match(pattern, msg)):
+		issue_id = msg.split(' ')[1]
 
-        items = get_issue(issue_id)
-        if items is None:
-            text = 'No existe la issue {}'.format(issue_id)
-        elif items[0] == 'closed':
-            text = 'La issue {} ya estaba cerrada.'.format(issue_id)
-        else:
-            text = close_issue(issue_id)
-            text += ' Para ver el issue correspondiente, ingrese a ' \
-                    'https://github.com/Genaron/T07/issues/{}' \
-                    ''.format(issue_id)
-        bot.send_message(chat_id=update.message.chat_id, text=text)
-    else:
-        cmd_error(bot, update)
-
-
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
-
-get_handler = CommandHandler('get', get)
-dispatcher.add_handler(get_handler)
-
-post_handler = CommandHandler('post', post)
-dispatcher.add_handler(post_handler)
-
-label_handler = CommandHandler('label', label)
-dispatcher.add_handler(label_handler)
-
-close_handler = CommandHandler('close', close)
-dispatcher.add_handler(close_handler)
-
-updater.start_polling()
+		items = get_issue(issue_id)
+		if items is None:
+			text = 'No existe la issue {}'.format(issue_id)
+		elif items[0] == 'closed':
+			text = 'La issue {} ya estaba cerrada.'.format(issue_id)
+		else:
+			text = close_issue(issue_id)
+			text += ' Para ver el issue correspondiente, ingrese a ' \
+					'https://github.com/Genaron/T07/issues/{}' \
+					''.format(issue_id)
+		bot.send_message(chat_id=update.message.chat_id, text=text)
+	else:
+		cmd_error(bot, update)
