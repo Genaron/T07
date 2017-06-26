73cd19478159e30634e346679414fbc04eb97dcb
Genaron <gslaymuns@uc.cl>
2017-06-26 07:02:05 -0400

    labels

--- mi_bot.py
+++ mi_bot.py
@@ -10,123 +10,127 @@ token_bot = '439953184:AAFdqVKqmxFOnKuClHhjCPN0v0Ee1ZmMwZw'
 updater = Updater(token=token_bot)
 dispatcher = updater.dispatcher
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - '
-							'%(message)s', level=logging.INFO)
+                           '%(message)s', level=logging.INFO)
 
 
 def start(bot, update):
-	text = 'Hola {}!\n'.format(update.message.from_user.first_name)
-	text += 'Soy PrograBot! Con mi ayuda, podrás manejar algunas funcional'
-	text += 'idades de los issues presentes en el repositorio https://github.co'
-	text += 'm/Genaron/T07\n-------------\nCOMANDOS:\n/get #num_issue:'
-	text += 'Se obtiene la información asociada a la issue correspondiente.\n'
-	text += '/post #num_issue *respuesta: Se responde la issue correspondiente '
-	text += 'con la respuesta entregada.\n/label #num_issue label: Se asigna la'
-	text += ' etiqueta indicada al issue correspondiente.\n/close #num_issue: '
-	text += 'se cierra la issue indicada.'
-	bot.send_message(chat_id=update.message.chat_id, text=text)
+    text = 'Hola {}!\n'.format(update.message.from_user.first_name)
+    text += 'Soy PrograBot! Con mi ayuda, podrás manejar algunas funcional'
+    text += 'idades de los issues presentes en el repositorio https://github.co'
+    text += 'm/Genaron/T07\n-------------\nCOMANDOS:\n/get #num_issue:'
+    text += 'Se obtiene la información asociada a la issue correspondiente.\n'
+    text += '/post #num_issue *respuesta: Se responde la issue correspondiente '
+    text += 'con la respuesta entregada.\n/label #num_issue label: Se asigna la'
+    text += ' etiqueta indicada al issue correspondiente.\n/close #num_issue: '
+    text += 'se cierra la issue indicada.'
+    bot.send_message(chat_id=update.message.chat_id, text=text)
 
 
 def cmd_error(bot, update):
-	text = 'El comando ingresado no es válido. Use /start para ver la lista '
-	text += 'de comandos.'
-	bot.send_message(chat_id=update.message.chat_id, text=text)
+    text = 'El comando ingresado no es válido. Use /start para ver la lista '
+    text += 'de comandos.'
+    bot.send_message(chat_id=update.message.chat_id, text=text)
 
 
 def get(bot, update):
-	comando = ''
-	for s in 'get':
-		comando += '({}|{})'.format(s, s.upper())
-	pattern = '^/{} \d+$'.format(comando)
+    comando = ''
+    for s in 'get':
+        comando += '({}|{})'.format(s, s.upper())
+    pattern = '^/{} \d+$'.format(comando)
 
-	msg = update.message.text
-	if bool(re.match(pattern, msg)):
-		issue_id = msg.split(' ')[1]
-		items = get_issue(issue_id)
-		if items is None:
-			text = 'No existe la issue {}'.format(issue_id)
-		else:
-			state, numero, autor, titulo, texto, url = items
-			if state == 'closed':
-				text = 'La issue {} ya fue resuelta.\n\n'.format(issue_id)
-			else:
-				text = 'La issue {} no ha sido resuelta.\n\n'.format(issue_id)
-			text += '[autor: {}]\n\n'.format(autor)
-			text += '[#{} - {}]\n\n'.format(numero, titulo)
-			text += texto + '\n\n' + url
-		bot.send_message(chat_id=update.message.chat_id, text=text)
-	else:
-		cmd_error(bot, update)
+    msg = update.message.text
+    if bool(re.match(pattern, msg)):
+        issue_id = msg.split(' ')[1]
+        items = get_issue(issue_id)
+        if items is None:
+            text = 'No existe la issue {}'.format(issue_id)
+        else:
+            state, numero, autor, titulo, texto, url = items
+            if state == 'closed':
+                text = 'La issue {} ya fue resuelta.\n\n'.format(issue_id)
+            else:
+                text = 'La issue {} no ha sido resuelta.\n\n'.format(issue_id)
+            text += '[autor: {}]\n\n'.format(autor)
+            text += '[#{} - {}]\n\n'.format(numero, titulo)
+            text += texto + '\n\n' + url
+        bot.send_message(chat_id=update.message.chat_id, text=text)
+    else:
+        cmd_error(bot, update)
 
 
 def post(bot, update):
-	user = update.message.from_user.first_name
+    user = update.message.from_user.first_name
 
-	comando = ''
-	for s in 'post':
-		comando += '({}|{})'.format(s, s.upper())
-	pattern = '^/{} \d+ (\s|\S)+'.format(comando)
+    comando = ''
+    for s in 'post':
+        comando += '({}|{})'.format(s, s.upper())
+    pattern = '^/{} \d+ (\s|\S)+'.format(comando)
 
-	msg = update.message.text
-	if bool(re.match(pattern, msg)):
-		space_1 = msg.find(' ')
-		space_2 = msg.find(' ', space_1 + 1)
-		issue_id = msg[space_1 + 1: space_2]
-		respuesta = msg[space_2 + 1:]
-		items = get_issue(issue_id)
-		if items is None:
-			text = 'No existe la issue {}'.format(issue_id)
-		else:
-			signature = '\n\n----------\nRespuesta generada por {} desde ' \
-						'@GenaroLaymunsBot de Telegram.'.format(user)
-			status = post_comment(issue_id, respuesta + signature)
-			if status == 201:
-				text = 'Issue número {} respondida con éxito! Para ver el ' \
-					   'issue con los comentarios, ingrese a https://github' \
-					   '.com/Genaron/T07/issues/{}'.format(issue_id, issue_id)
-			else:
-				text = 'No fue posible comentar el issue.'
-		bot.send_message(chat_id=update.message.chat_id, text=text)
-	else:
-		cmd_error(bot, update)
+    msg = update.message.text
+    if bool(re.match(pattern, msg)):
+        space_1 = msg.find(' ')
+        space_2 = msg.find(' ', space_1 + 1)
+        issue_id = msg[space_1 + 1: space_2]
+        respuesta = msg[space_2 + 1:]
+        items = get_issue(issue_id)
+        if items is None:
+            text = 'No existe la issue {}'.format(issue_id)
+        else:
+            signature = '\n\n----------\nRespuesta generada por {} desde ' \
+                        '@GenaroLaymunsBot de Telegram.'.format(user)
+            status = post_comment(issue_id, respuesta + signature)
+            if status == 201:
+                text = 'Issue número {} respondida con éxito! Para ver el ' \
+                       'issue con los comentarios, ingrese a https://github' \
+                       '.com/Genaron/T07/issues/{}'.format(issue_id, issue_id)
+            else:
+                text = 'No fue posible comentar el issue.'
+        bot.send_message(chat_id=update.message.chat_id, text=text)
+    else:
+        cmd_error(bot, update)
 
 
 def label(bot, update):
-	comando = ''
-	for s in 'label':
-		comando += '({}|{})'.format(s, s.upper())
-	pattern = '^/{} \d+ \w+$'.format(comando)
+    comando = ''
+    for s in 'label':
+        comando += '({}|{})'.format(s, s.upper())
+    pattern = '^/{} \d+ \w+$'.format(comando)
 
-	msg = update.message.text
-	if bool(re.match(pattern, msg)):
-		space_1 = msg.find(' ')
-		space_2 = msg.find(' ', space_1 + 1)
-		issue_id = msg[space_1 + 1: space_2]
-		new_label = msg[space_2 + 1:]
+    msg = update.message.text
+    if bool(re.match(pattern, msg)):
+        space_1 = msg.find(' ')
+        space_2 = msg.find(' ', space_1 + 1)
+        issue_id = msg[space_1 + 1: space_2]
+        new_label = msg[space_2 + 1:]
 
-		items = get_issue(issue_id)
-		if items is None:
-			text = 'No existe la issue {}'.format(issue_id)
-		else:
-			text = add_label(issue_id, new_label)
-		bot.send_message(chat_id=update.message.chat_id, text=text)
-	else:
-		cmd_error(bot, update)
+        items = get_issue(issue_id)
+        if items is None:
+            text = 'No existe la issue {}'.format(issue_id)
+        else:
+            text = add_label(issue_id, new_label)
+            if text[0] != 'N':
+                text += ' Para ver el issue con los comentarios, ingrese a ' \
+                        'https://github.com/Genaron/T07/issues/{}' \
+                        ''.format(issue_id)
+        bot.send_message(chat_id=update.message.chat_id, text=text)
+    else:
+        cmd_error(bot, update)
 
 
 # todo
 def close(bot, update):
-	comando = ''
-	for s in 'close':
-		comando += '({}|{})'.format(s, s.upper())
-	pattern = '^/{} \d+$'.format(comando)
+    comando = ''
+    for s in 'close':
+        comando += '({}|{})'.format(s, s.upper())
+    pattern = '^/{} \d+$'.format(comando)
 
-	msg = update.message.text
-	if bool(re.match(pattern, msg)):
-		issue_id = msg.split(' ')[1]
-		text = 'Cerrando issue número: {}'.format(issue_id)
-		bot.send_message(chat_id=update.message.chat_id, text=text)
-	else:
-		cmd_error(bot, update)
+    msg = update.message.text
+    if bool(re.match(pattern, msg)):
+        issue_id = msg.split(' ')[1]
+        text = 'Cerrando issue número: {}'.format(issue_id)
+        bot.send_message(chat_id=update.message.chat_id, text=text)
+    else:
+        cmd_error(bot, update)
 
 
 start_handler = CommandHandler('start', start)
