56a3c219a5af0487be1dc453f8f6f12e502335b6
Genaron <gslaymuns@uc.cl>
2017-06-26 02:44:13 -0400

    aa

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ from flask import Flask
 app = Flask(__name__)
 
 
+@app.route('/')
+def index():
+	return 'El servidor está arriba!'
+
+
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8080))
-    app.run(host='https://heroku-app-t07.herokuapp.com/', port=port)
+	port = int(os.environ.get('PORT', 8080))
+	app.run(host='https://heroku-app-t07.herokuapp.com/', port=port)
